bd898af787d3619692d4ede6493105d73c198159
Luiza B. Laquini <72242547+luizalaquini@users.noreply.github.com>
2022-08-26 16:32:35 -0300

    Add files via upload

---
+++ 02 - If/elif.py
@@ -0,0 +1,10 @@
+"""
+
+if condição:
+    o que fazer se a condição 1 for verdadeira
+elif condição_2:
+    o que fazer se a condição 1 for falsa e a condição 2 for verdadeira
+else:
+    o que fazer se a condição 1 e a condição 2 forem falsas
+
+"""
