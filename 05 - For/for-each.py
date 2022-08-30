cd4dce68f899ba8f05726f2895461616cb3daa00
Luiza B. Laquini <72242547+luizalaquini@users.noreply.github.com>
2022-08-29 22:49:44 -0300

    add for

---
+++ 05 - For/for-each.py
@@ -0,0 +1,12 @@
+for i in range(5):
+  print (i)
+
+produtos = ['coca', 'fanta', 'sprite', 'guaraná', 'pepsi']
+
+for produto in produtos:
+  print (produto)
+
+texto = 'luiza@gmail.com'
+
+for ch in texto:
+  print (ch)
